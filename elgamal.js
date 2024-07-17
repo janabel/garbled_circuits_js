e10ad576d42fab0d35e04349e54ac1338a769174
janabel <janabeltxia@gmail.com>
2024-07-16 20:42:29 -0700

    fix names of elements

--- elgamal.js
+++ elgamal.js
@@ -1,14 +1,16 @@
 import ElGamal from "elgamal";
 
-document.getElementById("keygen-button").addEventListener("click", function () {
-  const elgamal = ElGamal.generateAsync(14);
-  const priv_key = elgamal.x;
-  const public_key = elgamal.y;
+document
+  .getElementById("key-gen-button")
+  .addEventListener("click", function () {
+    const elgamal = ElGamal.generateAsync(14);
+    const priv_key = elgamal.x;
+    const public_key = elgamal.y;
 
-  document.getElementById(
-    "private-key"
-  ).innerText = `Your private key is: ${priv_key}`;
-  document.getElementById(
-    "public-key"
-  ).innerText = `Your public key is: ${public_key}`;
-});
+    document.getElementById(
+      "private-key"
+    ).innerText = `Your private key is: ${priv_key}`;
+    document.getElementById(
+      "public-key"
+    ).innerText = `Your public key is: ${public_key}`;
+  });
