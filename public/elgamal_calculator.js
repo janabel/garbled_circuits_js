2ea2cc5d6a3a3521d4ef5153114b476f5c2e5296
janabel <janabeltxia@gmail.com>
2024-07-16 21:17:13 -0700

    move files to public

---
+++ public/elgamal_calculator.js
@@ -0,0 +1,17 @@
+// import ElGamal from "elgamal";
+
+document
+  .getElementById("key-gen-button")
+  .addEventListener("click", function () {
+    console.log("generating keypair...");
+    const elgamal = ElGamal.generateAsync(14);
+    const priv_key = elgamal.x;
+    const public_key = elgamal.y;
+
+    document.getElementById(
+      "private-key"
+    ).innerText = `Your private key is: ${priv_key}`;
+    document.getElementById(
+      "public-key"
+    ).innerText = `Your public key is: ${public_key}`;
+  });
