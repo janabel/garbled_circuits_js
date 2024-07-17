0ab9d4157d609c3f099135980c0e7143c44e2717
Brian Lawrence <tideofwords@gmail.com>
2024-07-17 15:04:27 -0700

    elgamal import works

--- temp/index.js
+++ temp/index.js
@@ -1,4 +1,14 @@
-const elgamal = require("../node_modules/elgamal/lib/elgamal.js");
+const elgamal = require("../node_modules/elgamal/lib/elgamal.js").default;
 
 console.log("Index yay");
 console.log(elgamal);
+
+const keygenButton = document.getElementById("keygen-button");
+keygenButton.addEventListener("click", async function () {
+    const eg = await elgamal.generateAsync(); // Recommended way of initialization
+    const secret = 'The quick brown fox jumps over the lazy dog';
+    const encrypted = await eg.encryptAsync(secret);
+    const decrypted = await eg.decryptAsync(encrypted);
+    
+    console.log(decrypted.toString() === secret); // true
+});
