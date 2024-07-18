fe6cd57e156371f8ff9a420cff4488ed02608d81
janabel <janabeltxia@gmail.com>
2024-07-17 17:55:34 -0700

    fix t ypoe in index.js encryptAsync

--- temp/index.js
+++ temp/index.js
@@ -1,24 +1,20 @@
 const elgamal = require("../node_modules/elgamal/lib/elgamal.js").default;
 
-console.log("Index yay");
-console.log(elgamal);
-
 const keygenButton = document.getElementById("keygen-button");
 keygenButton.addEventListener("click", async function () {
   const eg = await elgamal.generateAsync(); // Recommended way of initialization
+
   const secret = "The quick brown fox jumps over the lazy dog"; // Message to encrypt
 
   const priv_key = await eg.x;
   const public_key = await eg.y;
-  console.log("priv key = " + priv_key);
-  console.log("pub key = " + public_key);
-  console.log("hi");
 
   document.getElementById("keygen-out-priv").innerText += " " + priv_key;
   document.getElementById("keygen-out-public").innerText += " " + public_key;
 
-  // const encrypted = await eg.encryptAnsync(secret);
-  // const decrypted = await eg.decryptAsync(encrypted);
+  const encrypted = await eg.encryptAsync(secret);
+  const decrypted = await eg.decryptAsync(encrypted);
 
-  // console.log(decrypted.toString() === secret); true
+  console.log(decrypted.toString() === secret);
+  true;
 });
