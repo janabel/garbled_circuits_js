971ecd507154f153c126acf2ceeb42a30b9d3242
Brian Lawrence <tideofwords@gmail.com>
2024-07-18 11:50:46 -0700

    Test works.  Convert CT to string and back, then decrypt

--- public/elgamal.js
+++ public/elgamal.js
@@ -111,10 +111,25 @@ elGamalTestButton.addEventListener("click", async function() {
   console.log(testEg);
   const msg = "hi";
   const cpt = await testEg.encryptAsync(msg);
+  console.log("cpt string");
+  console.log(cpt.a);
+  console.log(cpt.b);
+  const aString = cpt.a.toString();
+  const bString = cpt.b.toString();
+  const aBigInt = new jsbn.BigInteger(aString.trim());
+  const bBigInt = new jsbn.BigInteger(bString.trim());
+  console.log(cpt.a.toString());
+  console.log(cpt.b.toString());
+  console.log("new bigints");
+  console.log(aBigInt);
+  console.log(bBigInt);
+  const cpt2 = new encryptedvalue(aBigInt, bBigInt);
   const msgDec = await testEg.decryptAsync(cpt);
+  const msgDec2 = await testEg.decryptAsync(cpt2);
   console.log("done with test");
   console.log(cpt);
   console.log(msgDec);
   console.log(msgDec.bi);
   console.log(msgDec.toString());
+  console.log(msgDec2.toString());
 })
