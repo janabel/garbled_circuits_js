d210be11e53d060f0184a2a5ef2e073198ffeb7a
Brian Lawrence <tideofwords@gmail.com>
2024-07-18 12:30:19 -0700

    Uncaught type error

--- public/elgamal.js
+++ public/elgamal.js
@@ -95,7 +95,11 @@ elGamalDecryptButton.addEventListener("click", async function () {
   console.log(ciphertextProcessed.a.toString());
   console.log(ciphertextProcessed.b.toString());
 
-  temp_eg = new elgamal(eg.p, eg.g, 0, privKeyInput); // placeholder public key
+  console.log("modpow?");
+  console.log(eg.g.modPow(eg.x, eg.p));
+  console.log(eg.y);
+  pubKey = eg.g.modPow(privKeyInput, eg.p);
+  temp_eg = new elgamal(eg.p, eg.g, pubKey, privKeyInput); // placeholder public key
   console.log("temp_eg = " + temp_eg);
   const message = await temp_eg.decryptAsync(ciphertextProcessed); // =(a,b), where a = g^r is the blinding factor, b = g^rx * m is the encryption
   const message2 = await eg.decryptAsync(ciphertextProcessed);
