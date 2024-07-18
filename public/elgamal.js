bc1db13efdad872503052c3f64f5e2599355902c
janabel <janabeltxia@gmail.com>
2024-07-18 11:01:01 -0700

    working on elgamal decryption

--- public/elgamal.js
+++ public/elgamal.js
@@ -1,4 +1,9 @@
 const elgamal = require("../node_modules/elgamal/lib/elgamal.js").default;
+const encryptedvalue =
+  require("../node_modules/elgamal/lib/models/encrypted-value.js").default;
+const jsbn = require("jsbn");
+const decryptedvalue =
+  require("../node_modules/elgamal/lib/models/decrypted-value.js").default;
 
 let eg = undefined; // to be remembered
 
@@ -6,18 +11,17 @@ const keygenButton = document.getElementById("keygen-button");
 keygenButton.addEventListener("click", async function () {
   eg = await elgamal.generateAsync(256); // Recommended way of initialization
 
-  const priv_key = await eg.x;
-  const public_key = await eg.y;
+  const privKeyOutput = await eg.x;
+  const publicKeyOutput = await eg.y;
 
-  document.getElementById("keygen-out-priv").innerText += " " + priv_key;
-  document.getElementById("keygen-out-public").innerText += " " + public_key;
+  document.getElementById("keygen-out-priv").innerText += " " + privKeyOutput;
+  document.getElementById("keygen-out-public").innerText +=
+    " " + publicKeyOutput;
 });
 
 const elGamalEncryptButton = document.getElementById("elg-enc-button");
 elGamalEncryptButton.addEventListener("click", async function () {
   const encOutput = document.getElementById("elg-enc-output");
-
-  console.log(typeof eg);
 
   if (typeof eg === "undefined") {
     console.log("eg still undefined!");
@@ -25,24 +29,76 @@ elGamalEncryptButton.addEventListener("click", async function () {
     return;
   }
 
-  const pubKey = Number(document.getElementById("pub-key").value); // if pass in new public key, just use existing p, g
+  const publicKeyInput = Number(document.getElementById("pub-key").value); // if pass in new public key, just use existing p, g
   const msg = document.getElementById("pub-enc-input").value;
 
-  if (!pubKey || !msg) {
+  if (!publicKeyInput || !msg) {
     document.getElementById("elg-enc-output").innerText =
       "Please enter both a public key and a message.";
     return;
   }
-  console.log(typeof pubKey);
 
-  temp_eg = new elgamal(await eg.p, await eg.g, pubKey, 0); // placeholder private key
-  const ciphertext = await temp_eg.encryptAsync(msg); // =(a,b), where a = g^r is the blinding factor, b = g^rx * m is the encryption
-  console.log(ciphertext.toString());
+  temp_eg = new elgamal(await eg.p, await eg.g, publicKeyInput, 0); // placeholder private key
+  const ciphertextOutput = await temp_eg.encryptAsync(msg); // =(a,b), where a = g^r is the blinding factor, b = g^rx * m is the encryption
   encOutput.innerText =
     "Your ciphertext (g^r, g^rs * m) is: " +
     "(" +
-    ciphertext.a.toString() +
+    ciphertextOutput.a.toString() +
     ", " +
-    ciphertext.b.toString() +
+    ciphertextOutput.b.toString() +
     ")";
+});
+
+async function parseOrderedPair(pairString) {
+  // Remove the parentheses
+  pairString = pairString.replace(/[()]/g, "");
+
+  // Split the string by the comma
+  const values = pairString.split(",");
+
+  // Convert the string values to numbers
+  const a = new jsbn.BigInteger(values[0].trim());
+  const b = new jsbn.BigInteger(values[1].trim());
+
+  const encryptedValue = new encryptedvalue(a, b);
+  return encryptedValue;
+}
+
+const elGamaDecryptButton = document.getElementById("elg-dec-button");
+elGamaDecryptButton.addEventListener("click", async function () {
+  const decOutput = document.getElementById("elg-dec-output");
+
+  if (typeof eg === "undefined") {
+    console.log("eg still undefined!");
+    decOutput.innerText = "Please generate a private public keypair first";
+    return;
+  }
+
+  const privKeyInput = Number(document.getElementById("private-key").value); // if pass in new private key, just use existing p, g
+  const ciphertextTextInput =
+    document.getElementById("private-dec-input").value;
+  if (!privKeyInput || !ciphertextTextInput) {
+    document.getElementById("elg-dec-output").innerText =
+      "Please enter both a private key and a ciphertext.";
+    return;
+  }
+
+  // otherwise parse privKeyInput into
+  let ciphertextProcessed = await parseOrderedPair(ciphertextTextInput);
+  if (!ciphertextProcessed.a || !ciphertextProcessed.b) {
+    document.getElementById("elg-dec-output").innerText =
+      "Please enter a valid ciphertext";
+    return;
+  }
+
+  console.log("type of a is " + typeof ciphertextProcessed.a);
+  console.log(ciphertextProcessed.a.toString());
+  console.log(ciphertextProcessed.b.toString());
+
+  temp_eg = new elgamal(eg.p, eg.g, 0, privKeyInput); // placeholder public key
+  console.log("temp_eg = " + temp_eg);
+  const message = await temp_eg.decryptAsync(ciphertextProcessed); // =(a,b), where a = g^r is the blinding factor, b = g^rx * m is the encryption
+  // const fakemsg = new decryptedvalue("10");
+  console.log("message is " + message.toString());
+  // decOutput.innerText = "Your plaintext is: " + message.toString();
 });
